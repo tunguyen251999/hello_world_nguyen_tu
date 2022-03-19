9de1a0e535d365ab341fa0b394421f402e589edf
root <root@localhost.localdomain>
2022-03-19 07:11:38 +0700

    modify convert file

--- convert.cpp
+++ convert.cpp
@@ -6,20 +6,21 @@ using namespace std;
 
 void convert(string &str){
     int len = str.length();
-
+    //convert according to ASCII tables
     for(int i = 0; i < len; i++){
         if(str[i]>= 'a' && str[i] <='z'){
-            str[i]=str[i]-32;
+            str[i]=str[i]-32; //convert lowercase to uppercase
         }
         else if (str[i]>='A'&& str[i]<='Z')
         {
-            str[i]=str[i]+32;
+            str[i]=str[i]+32; //convert uppercase to lowercase
         }
 
     }
 }
 int main(){
     string str = "nguyen tu";
+    //call function
     convert(str);
 
     cout << str;
